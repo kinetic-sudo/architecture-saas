544afcc9f6bdf42b5755566f139a9b2e0a7f86a5
kinetic4 <pratyushbhargava6@gmail.com>
2026-02-22 23:50:48 +0530

    Enhance styling and layout by implementing a new theme in styles.css, adding a Navbar component to the main App, and updating the RootDocument structure. This update improves visual consistency and user experience across the application.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import { createFileRoute } from '@tanstack/react-router'
 import {
   Zap,
@@ -51,8 +52,11 @@ function App() {
   ]
 
   return (
+    <div className='home'>
+      <Navbar />
     <h1 className="text-3xl text-indigo-700 font-extrabold">
       Home
     </h1>
+    </div>
   )
 }
