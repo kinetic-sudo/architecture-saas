78ec7ab9da4675683313f39b440d71d72a227376
kinetic4 <pratyushbhargava6@gmail.com>
2026-02-22 23:54:57 +0530

    Refactor Navbar component to include a structured header with branding elements. This update enhances the layout and visual appeal of the navigation bar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,25 @@
 import { Link } from '@tanstack/react-router'
 
 import { useState } from 'react'
-import { Home, Menu, X } from 'lucide-react'
+import { Box, Home, Menu, X } from 'lucide-react'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
     <>
-    Navbar
+    <header className='navbar'>
+      <nav className='inner'>
+        <div className="left">
+          <div className="brand">
+            <Box className='logo'/>
+            <span className='name'>
+              Roomify
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
     </>
   )
 }
