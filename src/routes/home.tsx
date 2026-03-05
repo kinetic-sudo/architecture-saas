cb8eedd7c2a4c79872a52f0e3faecadbb1ce33e5
kinetic4 <pratyushbhargava6@gmail.com>
2026-03-06 00:29:51 +0530

    Refactor project card in Home component of home.tsx to improve structure and display; reintroduce badge and update project title for clarity.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -67,25 +67,16 @@ export default function Home() {
                                   <img  src='https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png' alt="Project" 
                                   />
 
-                                  {/* <div className="badge">
-                                      <span>Community</span>
-                                  </div> */}
+                                  <div className="badge">
+                                    <span>Community</span>
+                                  </div>
                               </div>
 
-                              {/* <div className="card-body">
-                                  <div>
-                                      <h3>{name}</h3>
-
-                                      <div className="meta">
-                                          <Clock size={12} />
-                                          <span>{new Date(timestamp).toLocaleDateString()}</span>
-                                          <span>By JS Mastery</span>
-                                      </div>
-                                  </div>
-                                  <div className="arrow">
-                                      <ArrowUpRight size={18} />
-                                  </div>
-                              </div> */}
+                             <div className="card-body">
+                              <div>
+                                <h3>Project Manhattan</h3>
+                              </div>
+                             </div>
                           </div>
                   </div>
       </div>
