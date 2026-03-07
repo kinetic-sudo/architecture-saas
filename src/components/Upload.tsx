ce437a86923d33a28fb1a62cf47ff15a7eeae9d9
kinetic4 <pratyushbhargava6@gmail.com>
2026-03-07 17:18:26 +0530

    Update Upload component in Upload.tsx to include file presence indication and progress state management, enhancing user feedback during file uploads.

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,13 +1,25 @@
+import { useAuth } from '@/App'
 import React,{useState} from 'react'
 
 const Upload = () => {
 
     const [file, setFile] = useState<File | null>(null)
     const [isDragging, setIsDragging] = useState(false)
+    const [progress, setIsProgress] = useState(0)
+
+    const { isSignedIn } = useAuth()
 
   return (
     <div className='upload'>
-
+        {!file ? (
+            <div>
+                No file
+            </div>
+        ) : (
+            <div>
+                File
+            </div>
+        )}
     </div>
   )
 }
